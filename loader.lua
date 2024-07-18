777f8efe29d8ddd74c0e644285c7dc23804fa593
loader-live <loader-live@ani.yt>
2024-07-18 02:43:36 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
 local Rivals = "18f1dd13af3e82c67c6403c416ebb1a5"
-loadstring(game:HttpGet(("https://api.luarmor.net/files/v3/loaders/%s.lua"):format(Rivals))()
+loadstring(game:HttpGet(("https://api.luarmor.net/files/v3/loaders/%s.lua"):format(Rivals)))()
