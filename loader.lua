9ba1626b4446a1d99e9ffc76b6c729723ad58341
loader-live <loader-live@ani.yt>
2024-08-25 10:10:36 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,9 @@
-local Rivals = "18f1dd13af3e82c67c6403c416ebb1a5"
-loadstring(game:HttpGet(("https://api.luarmor.net/files/v3/loaders/%s.lua"):format(Rivals)))()
+function ImportHTTP(url)
+    loadstring(game:HttpGet(url))()
+end
+
+if game.PlaceId == 17625359962 then
+    ImportHTTP("https://api.luarmor.net/files/v3/loaders/18f1dd13af3e82c67c6403c416ebb1a5.lua")
+elseif game.PlaceId == 3837841034 then
+    ImportHTTP("https://api.luarmor.net/files/v3/loaders/152959ae911ebd24878d69548151cd21.lua")
+end
